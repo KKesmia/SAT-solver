0f4f12f9de821ddc1f7938790cf8f01e053c5cd4
KKesmia <48242134+KKesmia@users.noreply.github.com>
2019-09-17 13:42:21 +0100

    Update Try.java

--- Exacte/Try.java
+++ Exacte/Try.java
@@ -3,34 +3,32 @@ package Exacte;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-
 public class Try {
 
 	public static void main(String[] args) throws IOException {
-		
-		
 		long start;
-	
+	      	
+		// in case of attemoting to figure out the performence according to time.
+		// this though reaquires you to download the benchmark files and update the path below.
+		// Idealy to test an algorithm according to time we do not use print instructions so feel free to swap em out
 		for (int i=1; i<=100; i++) {
-			Reader_Cnf_Exc Book = new Reader_Cnf_Exc("C:\\Users\\Moi\\Desktop\\test_SAT\\uf20-0"+i+".cnf");
+			Reader_Cnf_Exc Book = new Reader_Cnf_Exc("PATH UPDATE HERE"+i+".cnf");
 			//Book.traverseInOrder(Book.getT());
 			Methods met = new Methods(Book.getT(), Book.getVar_num(), Book.getClau_size(), 
 			Book.getM(), Book.getClau_num());
 			start = System.currentTimeMillis(); 
-			//System.out.println("Depth first :");
-			ArrayList<Integer> r = met.SRP();
+			
+			//Depth first
+			//System.out.println(met.SRP());
+			
+			//Heuristic
+			//System.out.println(met.RA());
+			
+			//Breadth first
+			//System.out.println(met.RL());
+			
 			System.out.print((System.currentTimeMillis() - start )+",");
 		}
-	
-		
-		/*
-		System.out.println("Heuristic :");
-		System.out.println(met.RA());
-		
-		
-		System.out.println("Breadth first :");
-		System.out.println(met.RL());*/
 	}
 
 }
