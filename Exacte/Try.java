9bf08519e8af065d8eea2c9e808be3cd168142f1
KKesmia <48242134+KKesmia@users.noreply.github.com>
2019-09-17 13:43:48 +0100

    Update Try.java

--- Exacte/Try.java
+++ Exacte/Try.java
@@ -13,7 +13,10 @@ public class Try {
 		// Idealy to test an algorithm according to time we do not use print instructions so feel free to swap em out
 		for (int i=1; i<=100; i++) {
 			Reader_Cnf_Exc Book = new Reader_Cnf_Exc("PATH UPDATE HERE"+i+".cnf");
+			
+			// u can display the tree of solutions by decommenting this instruction
 			//Book.traverseInOrder(Book.getT());
+			
 			Methods met = new Methods(Book.getT(), Book.getVar_num(), Book.getClau_size(), 
 			Book.getM(), Book.getClau_num());
 			start = System.currentTimeMillis(); 
