c99de0176dc3ef6563b281fcf43292fedbaaf15b
KKesmia <48242134+KKesmia@users.noreply.github.com>
2019-09-17 14:18:27 +0100

    Update Try.java

--- PSO/Try.java
+++ PSO/Try.java
@@ -13,12 +13,15 @@ public class Try {
 		double[] w = {0.2, 0.4, .6, .8 , 1, 1.2, 1.4, 1.6, 1.8, 2};
 		
 		int[] var_popu = { 250, 500 , 750, 1000};
-		int[] var_gene = {1000, 2000, 3000 , 4000, 5000};
-		
+		int[] var_gene = {1000, 2000, 3000 , 4000, 5000}; // generations
 		PSO start;
-		//public PSO(double w, double c1, double c2, int iterations, int population, String path)
-
-		/*
+		
+		// default values w = 1, c1 = 1.25, c2 = 1.25, generations = 1000, population = 200
+		
+		// This is parameters settings and we process with the benchmark with 100 instance
+		// repeated executions of a set of parameters , for each loop we vary one of them and fix the rest to the default values 
+		
+		/*		
 		System.out.println("c1");
 		for(double c :c1) {
 			start = new PSO(1,c, 1.25, 1000, 200, "C:\\Users\\Moi\\Desktop\\test_SAT\\100\\uf100-01.cnf");
@@ -46,7 +49,7 @@ public class Try {
 		}
 		
 		
-		System.out.println("popu/gene");
+		System.out.println("population/generations");
 		for(int c :var_popu) {
 			for(int x :var_gene) {
 				start = new PSO(1, 1.25, 1.25, x, c, "C:\\Users\\Moi\\Desktop\\test_SAT\\100\\uf100-01.cnf");
@@ -56,8 +59,8 @@ public class Try {
 			}
 		}*/
 		
-		
-		long stt;
+		// testing according to time using instance of 150 and going through all the 100 files
+		long timestart;
 		String c;
 		int i = 1;
 		System.out.println("all files");
@@ -65,7 +68,7 @@ public class Try {
 			start = new PSO("C:\\Users\\Moi\\Desktop\\test_SAT\\uf150-0"+i+".cnf");
 			stt = System.currentTimeMillis(); 
 			c = start.Execution();
-			System.out.print((System.currentTimeMillis() - stt )+",");
+			System.out.print((System.currentTimeMillis() - timestart )+",");
 			i++;
 		}
 		
